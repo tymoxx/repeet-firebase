9d9c5b23c9beb41d6fa949042a0c56a27aa9f2b1
Yuriy <7777kara@gmail.com>
2022-09-08 23:23:18 +0200

    new webhook url

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,12 @@
 const functions = require("firebase-functions");
 const request = require("request");
 
+const WEBHOOK_URL = "https://hooks.slack.com/services/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";
 
 exports.notifyNewSignup = functions.auth.user().onCreate((user, context) => {
     const {email, uid, providerData} = user;
 
-    return request.post("https://hooks.slack.com/services/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", {
+    return request.post(WEBHOOK_URL, {
         json: {
             text: `✨ New signup
             ${email}
@@ -22,7 +23,7 @@ exports.notifyNewSignup = functions.auth.user().onCreate((user, context) => {
 exports.notifyUserDeleting = functions.auth.user().onDelete((user, context) => {
     const {email} = user;
 
-    return request.post("https://hooks.slack.com/services/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", {
+    return request.post(WEBHOOK_URL, {
         json: {
             text: `👋 User deleted his profile ${email}!`,
             channel: "#feed",
